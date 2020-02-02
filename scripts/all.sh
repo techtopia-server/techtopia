f413a019464d304cf3814cd0a415818d24fc5401
Morten Olsen <morten@olsen.pro>
2020-02-02 20:41:13 +0100

    Moved enable files outside of stacks

--- scripts/all.sh
+++ scripts/all.sh
@@ -5,7 +5,8 @@ ARGS=$@
 for CURRENT_DIR in "$STACK_LOCATION/"*/; do
   (
     CURRENT_DIR="$(realpath "$CURRENT_DIR")"
-    ENABLE_FILE="$CURRENT_DIR/enabled"
+    NAME="$(basename $CURRENT_DIR)"
+    ENABLE_FILE="$ROOT_DIR/enabled/$NAME"
     if [ -f "$ENABLE_FILE" ]; then
       bash "$SCRIPT_DIR/run.sh" "$CURRENT_DIR" $ARGS
     fi
--- scripts/list.sh
+++ scripts/list.sh
@@ -4,9 +4,9 @@ ARGS=$@
 
 for CURRENT_DIR in "$STACK_LOCATION/"*/; do
   (
-    CURRENT_DIR="$(realpath "$CURRENT_DIR")"
+    #CURRENT_DIR="$(realpath "$CURRENT_DIR")"
     NAME="$(basename $CURRENT_DIR)"
-    ENABLE_FILE="$CURRENT_DIR/enabled"
+    ENABLE_FILE="$ROOT_DIR/enabled/$NAME"
     if [ -f "$ENABLE_FILE" ]; then
       echo "✅  - $NAME"
     else
